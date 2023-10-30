26f437f97a29dab9959dada79a7273ee4cddb27b
Михаил Карпов <mihailkarpov@MacBook-Air-Mihail.local>
2023-10-30 22:02:16 +0300

    Initial commit

---
+++ src/ru/mirea/lab13/Number2/Number2Test.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab13.Number2;
+
+public class Number2Test {
+    public static void main(String[] args) {
+        Number2 person1 = new Number2("Кондратьев", "Павел", "Игоревич");
+        System.out.println(person1.getFullName());
+    }
+}
